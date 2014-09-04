e54ffdf88182c3cec8ec79728acc078a8629cdb8
Martyn Davis <mad@madlid.com>
2014-09-04 10:59:20 +1000

    Final working version - fixup down at load - state is determined by what is shown/hidden

--- source/scripts/techmeme-less.js
+++ source/scripts/techmeme-less.js
@@ -15,30 +15,29 @@
         return node;
     }
 
-    function hideNode(value) {
+    function displayNodes(value, display) {
         var i;
-        if (value instanceof Array) {
-            for (i = 0; i < value.length; i += 1) {
-                displayNode(value[i], 'none');
+        if (value) {
+            if (value instanceof Array) {
+                for (i = 0; i < value.length; i += 1) {
+                    displayNode(value[i], display);
+                }
+            } else {
+                displayNode(value, display);
             }
-        } else {
-            displayNode(value, 'none');
         }
+    }
+
+    function hideNode(value) {
+        displayNodes(value, 'none');
     }
 
     function showNode(value) {
-        var i;
-        if (value instanceof Array) {
-            for (i = 0; i < value.length; i += 1) {
-                displayNode(value[i], 'block');
-            }
-        } else {
-            displayNode(value, 'block');
-        }
+        displayNodes(value, 'block');
     }
 
-    function makeId(cluster, item, tag) {
-        return cluster + tag + item;
+    function getElement(cluster, item, tag) {
+        return document.getElementById(cluster + tag + item);
     }
 
     function findFirstChildWithClass(node, className) {
@@ -71,22 +70,6 @@
         }
         return children;
     }
-
-    // function countChildrenWithClass(node, className) {
-        // var count = 0,
-            // i;
-        // if (node && typeof node !== 'object') {
-            // node = document.getElementById(node);
-        // }
-        // if (node && node.hasChildNodes && node.hasChildNodes()) {
-            // for (i = 0; i < node.childNodes.length; i += 1) {
-                // if (node.childNodes[i].className === className) {
-                    // count += 1;
-                // }
-            // }
-        // }
-        // return count;
-    // }
 
     function countChildrenWithTag(node, tagName) {
         var count = 0,
@@ -146,24 +129,6 @@
         return text;
     }
 
-    // function logTree(node, indent) {
-        // var padding,
-            // i;
-        // if (node) {
-            // indent = indent || 0;
-            // padding = '';
-            // for (i = 0; i < indent; i += 1) {
-                // padding += ' ';
-            // }
-            // console.log(padding + 'Tag: ' + node.tagName + ' Type: ' + node.nodeType + ' Class: ' + node.className + ' Id: ' + node.id + ' Text: ' + node.innerText);
-            // if (node.hasChildNodes && node.hasChildNodes()) {
-                // for (i = 0; i < node.childNodes.length; i += 1) {
-                    // logTree(node.childNodes[i], indent + 2);
-                // }
-            // }
-        // }
-    // }
-
     function getCounts(node, counts) {
         var next = findFirstChildWithClass(node, 'dbptb'),
             child,
@@ -177,6 +142,7 @@
                 child = node.childNodes[i];
                 if (child.nodeType === 1) {
                     if (child.className === 'drhed') {
+                        type = undefined;
                         match = re.exec(child.innerText);
                         if (match && (match.length > 1)) {
                             type = match[1].toLowerCase();
@@ -186,6 +152,7 @@
                             counts = counts || { };
                             counts[type] = countChildrenWithTag(child, 'A');
                         }
+                        type = undefined;
                     } else {
                         type = undefined;
                     }
@@ -198,87 +165,72 @@
         return counts;
     }
 
-    // function isNodeShown(node) {
-        // return node && node.style && node.style.display === 'block';
-    // }
+    function isNodeShown(node) {
+        return node && node.style && node.style.display === 'block';
+    }
 
-    function createOnclick(dNode, pNode, dxNode, pxNode, doNodes, poNodes) {
-        return (function () {
-            var fixed = 0,
-                state;
-            return function () {
-                if (!fixed) {
-                    // Undo our mangling of the CSS to initially hide things
-                    showNode(doNodes);
-                    showNode(poNodes);
-                    fixed = 1;
-                }
-                if (!state) {
-                    showNode(dNode);
-                    hideNode(pNode);
-                    // Fixup 'more' vertical button height
-                    if (dNode && dxNode && dxNode.style) {
-                        dxNode.style.height = dNode.offsetHeight + 'px';
-                        dxNode.style.display = 'block';
-                    }
-                    state = 1;
-                } else if (state === 1) {
-                    hideNode(dNode);
-                    showNode(pNode);
-                    // Fixup 'more' vertical button height
-                    if (pNode && pxNode && pxNode.style) {
-                        pxNode.style.height = pNode.offsetHeight + 'px';
-                        pxNode.style.display = 'block';
-                    }
-                    state = 2;
-                } else {
-                    hideNode(dNode);
-                    hideNode(pNode);
-                    state = 0;
-                }
-            };
-        }());
+    function fixupHeight(node, xNode) {
+        // Fixup 'more' vertical button height
+        if (node && xNode && xNode.style) {
+            xNode.style.height = node.offsetHeight + 'px';
+        }
+    }
+
+    function createOnclick(dNode, pNode, dxNode, pxNode) {
+        return function () {
+            if (isNodeShown(dNode)) {
+                hideNode(dNode);
+                showNode(pNode);
+                fixupHeight(pNode, pxNode);
+            } else if (isNodeShown(pNode)) {
+                hideNode(dNode);
+                hideNode(pNode);
+            } else {
+                showNode(dNode);
+                hideNode(pNode);
+                fixupHeight(dNode, dxNode);
+            }
+        };
     }
 
     function addCountsToItem(cluster, item) {
-        var iNode = document.getElementById(makeId(cluster, item, 'i')),
-            captionNode = findFirstChildWithClass(iNode, 'ii'),
-            dNode = document.getElementById(makeId(cluster, item, 'd')),
-            pNode = document.getElementById(makeId(cluster, item, 'p')),
-            dxNode = document.getElementById(makeId(cluster, item, 'dx')),
-            pxNode = document.getElementById(makeId(cluster, item, 'px')),
+        var iNode = getElement(cluster, item, 'i'),
+            iiNode = findFirstChildWithClass(iNode, 'ii'),
+            dNode = getElement(cluster, item, 'd'),
+            pNode = getElement(cluster, item, 'p'),
+            dxNode = getElement(cluster, item, 'dx'),
+            pxNode = getElement(cluster, item, 'px'),
             doNodes = findChildrenWithClass(dNode, 'dbpt'),
             poNodes = findChildrenWithClass(pNode, 'dbpt'),
             countsNode,
             counts,
-            recover;
+            recover = 1;
 
-        if (captionNode) {
+        if (iiNode) {
+            // Ensure the summary and verbose information are both hidden
+            hideNode(dNode);
+            hideNode(pNode);
+            showNode(doNodes);  // These were hidden by our CSS munge
+            showNode(poNodes);  // These were hidden by our CSS munge
+
             if (doNodes && (doNodes.length > 0)) {
                 counts = getCounts(doNodes[0]);
-            }
-            if (counts) {
-                if (counts.more || counts.tweets) {
-                    countsNode = createElement('span', { text: makeCountsText(counts.more, counts.tweets),
-                                                         className: 'techmemeless',
-                                                         tooltip: "Show/hide 'More' and/or 'Tweets' information",
-                                                         onclick: createOnclick(dNode, pNode, dxNode, pxNode, doNodes, poNodes)});
-                    captionNode.appendChild(countsNode);
+                if (counts) {
+                    if (counts.more || counts.tweets) {
+                        countsNode = createElement('span', { text: makeCountsText(counts.more, counts.tweets),
+                                                             className: 'techmemeless',
+                                                             tooltip: "Show/hide 'More' and/or 'Tweets' information",
+                                                             onclick: createOnclick(dNode, pNode, dxNode, pxNode)});
+                        iiNode.appendChild(countsNode);
+                    }
+                    recover = 0;
                 }
-            } else {
-                recover = 1;
             }
-        } else {
-            recover = 1;
         }
 
         if (recover) {
-            if (dNode) {
-                showNode(dNode);
-            }
-            if (pNode) {
-                showNode(pNode);
-            }
+            showNode(dNode);
+            hideNode(pNode);
         }
 
         return iNode;
